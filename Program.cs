9199e0537e231a708586ef827cdbc50afc34b423
Dkingsboro <99dman@gmail.com>
2023-12-11 14:34:43 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Diagnostics.Metrics;
+using static System.Console;
+namespace LiquidStorageTankSim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StorageTank st1 = new StorageTank(); // Creates new storage tank st1 with no argument constructor with the values defines below st1.Length = 6.5;
+            st1.Width = 7;
+            st1.Height = 8;
+
+            WriteLine($"Capacity of the tank = {st1.Capacity():F4} gallons"); // Displays the complete capacity of the tank. This information is calculated using the Capacity method
+
+            if(st1.Add(50))  // Calls Add method to tell if 50 gallons is added to the tank, is it possible or not.Decides in a boolean.
+            {
+                WriteLine("50 gallons added to the tank");
+            }
+            else
+            {
+                WriteLine("Failed to add 50 gallons");
+            }
+
+            if (st1.Remove(30)) // Calls Remove method to decide if the proposed gallons are able to be removed.If not, provides a fail statement
+            {
+                WriteLine("30 gallons removed from the tank");
+            }
+            else
+            {
+                WriteLine("Failed to remove 30 gallons");
+            }
+
+            if (st1.Remove(70)) // Calls Remove method to decide if the proposed gallons are able to be removed.If not, provides a fail statement.
+            {
+                WriteLine("Failed to remove 70 gallons");
+            }
+            else
+            {
+                WriteLine("Not enough liquid to remove 70 gallons");
+            }
+
+            WriteLine("Current gallons after all add and remove = " + st1.CurrentGallons); // Displays how much liquid is in the tank after addition and removal
+            WriteLine($"The tank is {st1.GetPercentFull():f2}% full"); // Displays how full the tank is after addition and removal in a percentage
+
+            // Displays ToString that is referenced in StorageTank Class
+            WriteLine(st1.ToString());
+
+            WriteLine("\n" + "\n" + "Test 2:" + "\n"); // Just creates some space between the two tests in order to clean up
+
+            // Test 2
+            StorageTank st2 = new StorageTank(7.5, 5, 10); // Creates new Storage Tank st2.
+
+            WriteLine("Capacity of the tank = " + st2.Capacity() + " gallons"); // Displays the complete capacity of the tank. This information is calculated using the Capacity method
+
+            if (st2.Add(120)) // Calls Add method to add liquid to the tank, and then caclulate percent full to display out.
+            {
+                WriteLine($"The tank is {st2.GetPercentFull():f4}% full");
+            }
+            else
+            {
+                WriteLine("Failed to add 120 gallons");
+            }
+
+            if (st2.Add(150)) // Calls Add method to add liquid to the tank, and then caclulate percent full to display out.
+            {
+                WriteLine($"The tank is {st2.GetPercentFull():f3}% full");
+            }
+            else
+            {
+                WriteLine("Failed to add 150 gallons");
+            }
+
+            WriteLine(st2.ToString()); // Displays ToString that is referenced in StorageTank Class
+        }
+    }
+}
---
+++ StorageTank.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiquidStorageTankSim
+{
+    internal class StorageTank
+    {
+        public double Width { get; set; } // Instance variables for LST Class
+        public double Length { get; set; }
+        public double Height { get; set; }
+        public double CurrentGallons;
+        const double conversion = 7.48052; // Constant variable for the conversion to gallons
+
+        public StorageTank() // No argument constructor for the StorageTank class
+        {
+            Width = 5;
+            Length = 5;
+            Height = 5;
+            CurrentGallons = 0;
+        }
+
+        public StorageTank(double Length, double Width, double Height) // Triple instance constructor overload for Storage Tank.Defines Length, Width, and Height.
+        {
+            this.Length = Length;
+            this.Width = Width;
+            this.Height = Height;
+        }
+
+        public double Capacity() // Method to calculate the volume of the tank in gallons
+        {
+            double volume = (Length * Width * Height) * conversion;
+            return volume;
+        }
+
+        private void SetCurrentGallons(double volume) // This set method, sets the volume AFTER conversion to gallons for the Capacity method.This is ONLY to be used to within the class.
+        {
+            CurrentGallons = volume;
+        }
+
+        public bool Add(double gallons) // Add method in order to take the added gallons defined in the main, and see if it will fit in the storage tank.Returns true or false.
+        {
+            double estimatedVolume = gallons + CurrentGallons;
+            if (estimatedVolume <= Capacity())
+            {
+                SetCurrentGallons(estimatedVolume);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Remove(double gallons) // Return method inorder to take the removed gallons defined in the main, and will see if removal is possible within the storage tank at the calculated capacity.
+        {
+            if (gallons <= CurrentGallons)
+            {
+                SetCurrentGallons(CurrentGallons - gallons);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double GetPercentFull() // Simply a method for calculating the filled capacity of the tank in percent.
+        {
+            return CurrentGallons / Capacity()* 100;
+        }
+
+        public override string ToString() // To string method to display the information about the defined storage tank.
+        {
+            return
+                $"_______________________________________________________________\n" +
+                $"The tank has a width of {Width} feet\n" +
+                $"The tank has a length of {Length} feet\n" +
+                $"The tank has a height of {Height} feet\n" +
+                $"The tank capacity is {Capacity()} gallons\n" +
+                $"This container has current gallons of {CurrentGallons} gallons\n" +
+                $"_______________________________________________________________";
+         }
+    }
+}
